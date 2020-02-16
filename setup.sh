5a5f1d46224711ab4c716f2e5818511b1c7f04e7
Magic <me@magiccashew.com>
2020-02-16 04:44:01 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -78,5 +78,28 @@ sudo dnf install -y libguestfs-tools
 sudo sed -i 's/#WaylandEnable=false/WaylandEnable=false/g' /etc/gdm/custom.conf
 sudo sed -i '/WaylandEnable=false/ i DefaultSession=gnome-xorg.desktop' /etc/gdm/custom.conf
 
+################################################################
+# JetBrains Toolbox
+USER_AGENT=('User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36')
+URL=$(curl 'https://data.services.jetbrains.com//products/releases?code=TBA&latest=true&type=release' -H 'Origin: https://www.jetbrains.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.8' -H "${USER_AGENT[@]}" -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://www.jetbrains.com/toolbox/download/' -H 'Connection: keep-alive' -H 'DNT: 1' --compressed | grep -Po '"linux":.*?[^\\]",' | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
+sudo wget -cO  /tmp/$(basename ${URL}) ${URL} --read-timeout=5 --tries=0
+sudo mkdir /opt/jetbrains-toolbox
+sudo tar -xzf /tmp/$(basename ${URL}) -C /opt/jetbrains-toolbox --strip-components=1
+cat <<EOT >> /tmp/jetbrains-toolbox.sh
+#!/bin/bash
+/opt/jetbrains-toolbox/jetbrains-toolbox
+EOT
+sudo mv /tmp/jetbrains-toolbox.sh /opt/jetbrains-toolbox/jetbrains-toolbox.sh
+sudo chmod -R +rwx /opt/jetbrains-toolbox
+sudo ln -s /opt/jetbrains-toolbox/jetbrains-toolbox.sh /usr/local/bin/jetbrains-toolbox
+sudo chmod -R +rwx /usr/local/bin/jetbrains-toolbox
+rm /tmp/$(basename ${URL})
+
+cat <<EOT >> /home/dbm/installStep2.sh
+#!/bin/bash
+sh -c "$(curl -sSL https://raw.githubusercontent.com/magicCashew/x/master/setup2.sh)"
+EOT
+chmod +r /home/dbm/installStep2.sh
+
 #The user needs to reboot to apply all changes.
 sudo reboot now
