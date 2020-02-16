ab62e15c533e19b5903f22940e16625d344fca91
Magic <me@magiccashew.com>
2020-02-16 04:16:20 -0500

    Update and rename setup2.sh to userSetup.sh

--- userSetup.sh
+++ userSetup.sh
@@ -59,30 +59,6 @@ gsettings set org.gnome.nautilus.list-view use-tree-view true
 gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/gnome/adwaita-timed.xml'
 gsettings set org.gnome.desktop.screensaver picture-uri 'file:///usr/share/backgrounds/gnome/adwaita-timed.xml'
 
-################################################################
-# JetBrains Toolbox
-function getLatestUrl() {
-USER_AGENT=('User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36')
-URL=$(curl 'https://data.services.jetbrains.com//products/releases?code=TBA&latest=true&type=release' -H 'Origin: https://www.jetbrains.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.8' -H "${USER_AGENT[@]}" -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://www.jetbrains.com/toolbox/download/' -H 'Connection: keep-alive' -H 'DNT: 1' --compressed | grep -Po '"linux":.*?[^\\]",' | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
-echo $URL
-}
-getLatestUrl
-FILE=$(basename ${URL})
-DEST=/home/dbm/Downloads/$FILE
-wget -cO  ${DEST} ${URL} --read-timeout=5 --tries=0
-DIR="/opt/jetbrains-toolbox"
-if sudo mkdir ${DIR}; then
-    sudo tar -xzf ${DEST} -C ${DIR} --strip-components=1
-fi
-sudo chmod -R +rwx ${DIR}
-sudo touch ${DIR}/jetbrains-toolbox.sh
-sudo echo '#!/bin/bash' >> $DIR/jetbrains-toolbox.sh
-sudo echo "$DIR/jetbrains-toolbox" >> $DIR/jetbrains-toolbox.sh
-
-sudo ln -s ${DIR}/jetbrains-toolbox.sh /usr/local/bin/jetbrains-toolbox
-sudo chmod -R +rwx /usr/local/bin/jetbrains-toolbox
-rm ${DEST}
-
 ####################################################
 # nvm Node Install
 sh -c "$(curl -sSL https://raw.githubusercontent.com/creationix/nvm/master/install.sh)"
@@ -95,6 +71,3 @@ npm install --unsafe-perm -g gulp grunt grunt-cli uglify-js yarn
 mkdir /home/dbm/Applications
 curl https://www.navicat.com/download/direct-download?product=navicat15-premium-en.AppImage&location=1 --output /home/dbm/Applications/navicat15-premium-en.AppImage
 chmod +x navicat15-premium-en.AppImage
-
-#The user needs to reboot to apply all changes.
-sudo reboot now
