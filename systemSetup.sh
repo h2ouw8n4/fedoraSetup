e5b3c73011861d7fdb93fd22f6f73b948a84ac00
Magic <me@magiccashew.com>
2020-02-23 14:46:30 -0500

    Update systemSetup.sh

--- systemSetup.sh
+++ systemSetup.sh
@@ -34,34 +34,14 @@ sudo dnf install -y ffmpeg
 sudo dnf install -y file-roller-nautilus 
 sudo dnf install -y fuse-exfat 
 sudo dnf install -y fuse-sshfs 
-sudo dnf install -y gimp 
-sudo dnf install -y gimp-data-extras 
-sudo dnf install -y gimp-dbp 
-sudo dnf install -y gimp-dds-plugin 
-sudo dnf install -y gimp-elsamuko
-sudo dnf install -y gimp-focusblur-plugin 
-sudo dnf install -y gimp-fourier-plugin 
-sudo dnf install -y gimp-high-pass-filter 
-sudo dnf install -y gimp-layer-via-copy-cut
-sudo dnf install -y gimp-lensfun 
-sudo dnf install -y gimp-lqr-plugin 
-sudo dnf install -y gimp-luminosity-masks 
-sudo dnf install -y gimp-paint-studio 
-sudo dnf install -y gimp-resynthesizer 
-sudo dnf install -y gimp-wavelet-decompose 
-sudo dnf install -y gimp-wavelet-denoise-plugin
 sudo dnf install -y git 
-sudo dnf install -y gmic-gimp 
 sudo dnf install -y gnome-shell-extension-dash-to-dock
 sudo dnf install -y gnome-shell-extension-topicons-plus
 sudo dnf install -y gnome-shell-extension-user-theme
-sudo dnf install -y GREYCstoration-gimp
 sudo dnf install -y gvfs-fuse
 sudo dnf install -y gvfs-mtp
 sudo dnf install -y gvfs-nfs
 sudo dnf install -y gvfs-smb
-sudo dnf install -y inkscape
-sudo dnf install -y keepassxc
 sudo dnf install -y nautilus-extensions
 sudo dnf install -y nautilus-image-converter
 sudo dnf install -y nautilus-search-tool
@@ -119,70 +99,6 @@ sudo sed -i '/WaylandEnable=false/ i DefaultSession=gnome-xorg.desktop' /etc/gdm
 wget -cO /tmp/displaylink.rpm https://github.com/displaylink-rpm/displaylink-rpm/releases/download/v5.2.14-3-rc1/fedora-31-displaylink-1.6.4-2.x86_64.rpm --read-timeout=5 --tries=0
 sudo dnf install -y /tmp/displaylink.rpm
 rm /tmp/displaylink.rpm
-
-################################################################
-# JetBrains
-sudo rm -rf /opt/phpstorm && sudo mkdir /opt/phpstorm && curl -sSL 'https://download.jetbrains.com/product?code=PS&latest&distribution=linux' | sudo tar xvz -C /opt/phpstorm --strip 1
-sudo rm -rf /opt/pycharm && sudo mkdir /opt/pycharm && curl -sSL 'https://download.jetbrains.com/product?code=PC&latest&distribution=linux' | sudo tar xvz -C /opt/pycharm --strip 1
-sudo rm -rf /opt/datagrip && sudo mkdir /opt/datagrip && curl -sSL 'https://download.jetbrains.com/product?code=DG&latest&distribution=linux' | sudo tar xvz -C /opt/datagrip --strip 1
-sudo rm -rf /opt/webstorm && sudo mkdir /opt/webstorm && curl -sSL 'https://download.jetbrains.com/product?code=WS&latest&distribution=linux' | sudo tar xvz -C /opt/webstorm --strip 1
-
-cat <<EOT >> /home/dbm/.local/share/applications/jetbrains-phpstorm.desktop
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=PHPStorm
-Icon=/opt/phpstorm/bin/phpstorm.svg
-Exec="/opt/phpstorm/bin/phpstorm.sh" %f
-Comment=The smartest PHP IDE
-Categories=Development;IDE;
-Terminal=false
-StartupWMClass=jetbrains-phpstorm
-EOT
-chmod +x /home/dbm/.local/share/applications/jetbrains-phpstorm.desktop
-
-cat <<EOT >> /home/dbm/.local/share/applications/jetbrains-pycharm.desktop
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=pyCharm
-Icon=/opt/pycharm/bin/pycharm.svg
-Exec="/opt/pycharm/bin/pycharm.sh" %f
-Comment=The smartest Python IDE
-Categories=Development;IDE;
-Terminal=false
-StartupWMClass=jetbrains-pycharm
-EOT
-chmod +x /home/dbm/.local/share/applications/jetbrains-pycharm.desktop
-
-cat <<EOT >> /home/dbm/.local/share/applications/jetbrains-datagrip.desktop
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=DataGrip
-Icon=/opt/datagrip/bin/datagrip.svg
-Exec="/opt/datagrip/bin/datagrip.sh" %f
-Comment=DataGrip 
-Categories=Development;IDE;
-Terminal=false
-StartupWMClass=jetbrains-datagrip
-EOT
-chmod +x /home/dbm/.local/share/applications/jetbrains-datagrip.desktop
-
-cat <<EOT >> /home/dbm/.local/share/applications/jetbrains-webstorm.desktop
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=WebStorm
-Icon=/opt/webstorm/bin/webstorm.svg
-Exec="/opt/webstorm/bin/webstorm.sh" %f
-Comment=The smartest JavaScript IDE
-Categories=Development;IDE;
-Terminal=false
-StartupWMClass=jetbrains-webstorm
-EOT
-chmod +x /home/dbm/.local/share/applications/jetbrains-webstorm.desktop
-
 
 ################################################################
 # Stage User Setup Script for Post Reboot
